a640438e40fe54b70ea6ad519b7c736141598e5c
emioiso <jijjjiijiijijiji@gmail.com>
2024-02-15 22:27:11 +0900

    Initial Commit

---
+++ thesoundofquiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  thesoundofquiz
+//
+//  Created by emi oiso on 2024/02/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
