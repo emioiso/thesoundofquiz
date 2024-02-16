c6e8a4a1fdb52588496f2ea87d126a6ac00f1d71
emioiso <jijjjiijiijijiji@gmail.com>
2024-02-16 14:24:29 +0900

    cococa touch classを対象のview controllerに紐付けしました。

---
+++ thesoundofquiz/ScoreViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ScoreViewController.swift
+//  thesoundofquiz
+//
+//  Created by emi oiso on 2024/02/16.
+//
+
+import UIKit
+
+class ScoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
